d18f595733c7372b7ddf2becc0b140187a7e9969
sjcockell <sjcockell@gmail.com>
2008-10-03 12:53:06 +0100

    Adding functionality to SignalPInterface.py Committer: sjcockell@gmail.com On branch master  Changes to be committed:
    	modified:   glycosylation/SignalPInterface.py

--- glycosylation/SignalPInterface.py
+++ glycosylation/SignalPInterface.py
@@ -1,12 +1,26 @@
 import sys
 import os
+import glycosylation.SequenceHandler as sh
 
 class SignalPInterface:
 	"methods for handling SignalP within project"
-	def __init__(self, sprot_code=None):
+	def __init__(self, sprot_code=None, outtype=None):
 		"sets variables for instance"
-		seq_file = "out/"+sprot_code+".seq"
-		signalp_command = "signalp -t euk "+seq_file
+		if not os.path.exists('out/'):
+			os.system('mkdir out/')
+		seq_file = os.path.abspath('out/')
+		seq_file = os.path.join(seq_file, sprot_code+".seq")			
+		if not os.path.exists(seq_file):
+			seqh = sh.SequenceHandler(sprot_code)
+			seqh.write_sequence_file()
+		if outtype is None:
+			self.signalp_command = "signalp -t euk "+seq_file
+		elif outtype == '':
 
 	def execute_signalp(self):
+		fin, fout = os.popen4(self.signalp_command)
+		result = fout.read()
+		return result
+
+	def parse_signalp(self, output=None):
 		
